442457bea3e17bb4cceab1d314cc401aedb009c5
sbarnwell88 <susy6688@gmail.com>
2017-07-17 13:48:48 -0400

    added about me and profile sections

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+$( document ).ready(function(){
+  // Show sideNav
+  $('.button-collapse').sideNav('show');
+  // Hide sideNav
+  $('.button-collapse').sideNav('hide');
+  // Destroy sideNav
+  $('.button-collapse').sideNav('destroy');
+});
